f55fb4bfd5eb01a728d4cc1355ae72f73b0dbac2
Fred Setra <fredericosetra@yahoo.com.br>
2020-01-06 00:46:42 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Guess The Number - A Python Game
+
+Guess The Number - A simple "Guess The Number" game written in Python for educational purposes.
+
+## How to Run
+
+To run this game, you need to pass on two parameters: START & END (Integers please :D)
+
+Example: 
+```bash
+python3 guessNumber.py 1 10
+```
+The program will take numbers 1 and 10 and generate a list of 10 numbers. 
+You then have to guess which number was selected by the computer.
+
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ guessNumber.py
@@ -0,0 +1,37 @@
+import sys
+import random
+
+def getNumber(start, end):
+	number = int(input(f'Type a number between {start} to {end}: '))
+	num = validateNumber(number, start, end)
+	return num
+
+def validateNumber(num, start, end):
+	validateNumber = False
+	while validateNumber == False:
+		if num < start:
+			print(f'Number must be greater or equal to {start}')
+			num = int(input(f'Type a number between {start} to {end}: '))
+		elif num > end:
+			print(f'Number must be less or equal to {end}')
+			num = int(input(f'Type a number between {start} to {end}: '))
+		else: 
+			validateNumber = True
+	return num
+
+
+if sys.argv[1]:
+	start = int(sys.argv[1])
+	end = int(sys.argv[2])
+	numberList = list(range(start, end+1))
+	winnerNumber = random.choice(numberList)
+	guessNumber = getNumber(start, end)
+	mistakes = 1
+	while guessNumber != winnerNumber:
+		print('Wrong!!! Try again!')
+		guessNumber = getNumber(start, end)
+		mistakes += 1
+	print(f'Alright! It took you {mistakes} trys to get the Winner Number!')
+else:
+	print("Type in 2 numbers for the range.")
+
